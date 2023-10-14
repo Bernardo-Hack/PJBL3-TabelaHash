4ee804fe7bbab50155b83c979463e9a5a0f92a5f
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-10-14 18:27:08 -0300

    Update MyHashTableEx.java
    Lógica das funções de search e delete descrita como comentário

--- MyHashTableEx.java
+++ MyHashTableEx.java
@@ -1,11 +1,11 @@
 public class MyHashTableEx <T> extends MyHashTable <T> {
-    protected T[] values;
+    protected MyLinkedList<T>[] values;
     protected MyLinkedList<String>[] keys;
+
     public MyHashTableEx(int size) {
         super(size);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     void insert(Object key, T value) {
         if (this.isAlmostFull()) {
@@ -16,11 +16,11 @@ public class MyHashTableEx <T> extends MyHashTable <T> {
 
         if (this.keys[keyPos] == null) {
             this.keys[keyPos] = new MyLinkedList<String>(key.toString());
-            this.values[keyPos] = (T) new MyLinkedList<T>(value);
+            this.values[keyPos] = new MyLinkedList<T>(value);
 
         } else if (this.keys[keyPos] != null) {
             this.keys[keyPos].addValue(key.toString());
-            ((MyLinkedList<T>) this.values[keyPos]).addValue(value);
+            this.values[keyPos].addValue(value);
         }
 
         this.quantItems++;
@@ -28,13 +28,24 @@ public class MyHashTableEx <T> extends MyHashTable <T> {
 
     @Override
     T delete(Object key) {
-        // TODO Auto-generated method stub
+        /*
+         * Parecido com o método de busca mas fazer uma pira parecida
+         * com o da árvore, de armazenar o nó anterior ao que será
+         * deletado, e substituir a referencia de next do nó
+         * (this.next = this.next.next)
+         */
         return super.delete(key);
     }
 
     @Override
     T search(Object key) {
-        // TODO Auto-generated method stub
+        int keyPos = getKeyPos(key);
+
+        /* 
+         * Iterar pelas 2 linked lists ao mesmo tempo (while por exemplo)
+         * checando a lista de chaves até encontrar o node correto
+         * então retornar o nó da lista de valores.
+         */
         return super.search(key);
     }
 }
