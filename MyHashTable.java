a02c061c2e9c35fa18f02091119cec2e1f162f81
Bernardo de Lima Hack <102930809+Bernardo-Hack@users.noreply.github.com>
2023-10-13 22:01:31 -0300

    Funções implementadas
    Funções:
    - insert
    - search
    - delete
    - print
    + funções adicionais.
    
    Co-Authored-By: Gabriela Ribeiro <104264294+gribeiro08@users.noreply.github.com>
    Co-Authored-By: EmyMatsu <132858538+EmyMatsu@users.noreply.github.com>

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-
 public class MyHashTable <T> {
     private T[] values;
     private String[] keys;
@@ -7,14 +5,14 @@ public class MyHashTable <T> {
 
     @SuppressWarnings("unchecked")
 
-    public MyHashTable(Class<T> ct, int size) {
+    public MyHashTable(int size) {
         this.quantItems = 0;
-        this.values = (T[]) Array.newInstance(ct, size);
+        this.values = (T[]) new Object[size];
         this.keys = new String[size];
     }
 
     private boolean isAlmostFull() {
-        if (this.getSize() >= this.keys.length * 0.75) {
+        if (this.quantItems >= this.keys.length * 0.75) {
             return true;
 
         } else {
@@ -24,14 +22,10 @@ public class MyHashTable <T> {
     }
 
     private int getSize() {
-        return this.quantItems;
+        return this.keys.length;
     }
 
-    void insert(T key, T value) {
-        if (this.isAlmostFull()) {
-            this.doubleSize();
-        }
-        
+    private int getKeyPos(Object key) {
         if (key instanceof String) {
             // Código de espalhamento de chave String(deve ter 5 letras)
             String auxStr = key.toString();
@@ -42,28 +36,130 @@ public class MyHashTable <T> {
                 auxInt += ascii * (2 * i);
             }
 
-            int pos = auxInt % this.getSize();
-            this.keys[pos] = key.toString();
-            this.values[pos] = value;
+            return auxInt % this.getSize();
+
+        } else if (key instanceof Integer){
+            int auxInt = ((int)key);
+            // Código de espalhamento de chave com Integer
+            return auxInt % this.getSize();
 
         } else {
-            // Código de espalhamento de chave com Integer
+            System.out.println("Tipo de chave informada inválida!");
+            return -1;
+        }
+    }
 
+    void insert(Object key, T value) {
+        if (this.isAlmostFull()) {
+            this.doubleSize();
+        }
+        
+        int keyPos = getKeyPos(key);
+
+        if (keys[keyPos] == "null" || keys[keyPos] == null) {
+            this.keys[keyPos] = key.toString();
+            this.values[keyPos] = value;
+            this.quantItems++;
+
+        } else {
+            int auxPos = keyPos + 1;
+
+            while (true) {
+                if (auxPos == this.getSize()) {
+                    auxPos = 0;
+                }
+
+                if (auxPos == keyPos) {
+                    System.out.println("Tabela está cheia! 💀");
+                    break;
+                }
+
+                if (keys[auxPos] == "null" || keys[auxPos] == null) {
+                    this.keys[keyPos] = key.toString();
+                    this.values[keyPos] = value;
+                    this.quantItems++;
+                    break;
+                } else {
+                    auxPos++;
+                }
+            }
+        }
+    }
+
+    T delete(T key) {
+        int keyPos = getKeyPos(key);
+
+        if (this.keys[keyPos] == key) {
+            T rValue = this.values[keyPos];
+            this.keys[keyPos] = "null";
+            this.values[keyPos] = null;
+            return rValue;
+
+        } else {
+            int auxPos = keyPos + 1;
+
+            while (auxPos != keyPos) {
+                if (auxPos == this.getSize()) {
+                    auxPos = 0;
+                }
+
+                if (keys[auxPos] == key) {
+                    T rValue = this.values[auxPos];
+                    this.keys[auxPos] = "null";
+                    this.values[auxPos] = null;
+                    return rValue;
+
+                } else {
+                    auxPos++;
+                }
+            }
+        }
+        System.out.println("Valor não está na tabela! 💀");
+        return null;
+    }
+
+    T search(T key) {
+        int keyPos = getKeyPos(key);
+
+        if (this.keys[keyPos] == key) {
+            return this.values[keyPos];
+
+        } else {
+            int auxPos = keyPos + 1;
+
+            while (auxPos != keyPos) {
+                if (auxPos == this.getSize()) {
+                    auxPos = 0;
+                }
+
+                if (keys[auxPos] == key) {
+                    return this.values[auxPos];
+
+                } else {
+                    auxPos++;
+                }
+            }
+        }
+        return this.values[keyPos];
+    }
+
+    @SuppressWarnings("unchecked")
+
+    private void doubleSize() {
+        // função para dobrar o tamanho da tabela
+        int size = this.keys.length * 2;
+        T[] newValues = (T[]) new Object[size];
+        String[] newKeys = new String[size];
+
+        for (int i = 0; i < size / 2; i++) {
+            if (this.keys[i] != null) {
+                newKeys[i] = this.keys[i];
+                newValues[i] = this.values[i];
+            }
         }
 
-        this.quantItems++;
-    }
-
-    T delete(T value) {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
-    }
-
-    void search(T value) {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
-    }
-
-    void doubleSize() {
-        // função para dobrar o tamanho da tabela
+        this.keys = newKeys;
+        this.values = newValues;
     }
 
     void print() {
