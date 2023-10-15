429a790eb5ab2a239f99ebbca1092a0d6a247459
Gabriela Ribeiro <gr08.ribeiro@gmail.com>
2023-10-15 15:01:59 -0300

    C9omparação de eficiência dos dois tipos de Hash :p

--- HashTest.java
+++ HashTest.java
@@ -1,20 +1,71 @@
 public class HashTest {
     public static void main(String[] args) {
-        MyHashTable<Aluno> table1 = new MyHashTable<>(7);
+        MyHashTable<Aluno> hash1 = new MyHashTable<>(7);
+        MyHashTableEx<Aluno> hash2 = new MyHashTableEx<>(7);
         Aluno[] alunos1 = criarAlunos();
 
+        
         for (Aluno aluno : alunos1) {
-            table1.insert(aluno.getMatricula(), aluno);
+            hash1.insert(aluno.getMatricula(), aluno);
         }
 
         for (Aluno aluno : alunos1) {
-            table1.insert(aluno.getMatricula(), aluno);
+            hash1.insert(aluno.getMatricula(), aluno);
+        }
+        
+        System.out.println("Hash 1");
+        hash1.print();
+        System.out.println("Quantidade de itens: " + hash1.getQuantItems());
+        
+        for (Aluno aluno : alunos1) {
+            hash2.insert(aluno.getMatricula(), aluno);
         }
 
-        table1.print();
-        System.out.println(table1.getQuantItems());
+        for (Aluno aluno : alunos1) {
+            hash2.insert(aluno.getMatricula(), aluno);
+        }
+
+        System.out.println("\n Hash 2");
+        hash2.print();
+        System.out.println("Quantidade de itens: " + hash2.getQuantItems());
+
+
+        //buscando no hash 1
+        long startHash1 = System.currentTimeMillis();
+
+        hash1.search(123456); 
+        hash1.search(456789);
+        hash1.search(789012);
+        hash1.search(398244);
+        hash1.search(398244);
+        
+
+        long finishHash1 = System.currentTimeMillis();
+        long totalHash1 = finishHash1 - startHash1;
+        System.out.println("Tempo: " + totalHash1);
+
+        //buscando no hash 2
+        long startHash2 = System.currentTimeMillis();
+
+        hash2.search(123456); 
+        hash2.search(456789);
+        hash2.search(789012);
+        hash2.search(398244);
+        hash2.search(398244);
+
+        long finishHash2 = System.currentTimeMillis();
+        long totalHash2 = finishHash2 - startHash2;
+        System.out.println("Tempo: " + totalHash2);
+
+        if(totalHash1 < totalHash2){
+            System.out.println("O primeiro tipo de Hash (joga pro proximo) é mais eficiente!!");
+        }else{
+            System.out.println("O segundo tipo de Hash (joga na lista) é mais eficiente!!");
+        }
+
+        
     }
-
+        
     static Aluno[] criarAlunos() {
         Aluno[] alunos = new Aluno[5];
         alunos[0] = new Aluno(123456, "FDE45", "João");
